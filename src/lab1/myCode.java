482805d2c7f4d619685b6087a0a80506dd51e077
mossswiss <moss.swiss@gmail.com>
2018-06-26 15:15:00 +0700

    Reupload

---
+++ src/lab1/myCode.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class myCode {
+
+    //main
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Swiss Limmahasukhon");
+        System.out.println("360411760005");
+        System.out.println("Rajamangala University of Technology Srivijaya");
+        System.out.println("MIT 421");
+
+
+
+
+
+
+
+    }//main
+}//
