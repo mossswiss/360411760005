88efa9a22a324689c830a8b996796ac7080447c1
MossSwiss <moss.swiss@gmail.com>
2018-10-16 16:23:56 +0700

    lab3Quiz3

--- src/lab3/Quiz3.java
+++ src/lab3/Quiz3.java
@@ -1,7 +1,37 @@
 package lab3;
 
+import java.util.Scanner;
+
 public class Quiz3 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //input
+        //Fahrenheit
+        System.out.print(" Fahrenheit?: ");
+        double Fahrenheit = scanner.nextDouble();
+        //Celsius
+        double Celsius = (5.0/9.0) * (Fahrenheit -32.0);
+
+        //output
+        System.out.println("Celsius is"+Celsius);
+
+        System.out.println("Celsius is"+Celsius);
+        System.out.println("Celsius is"+Celsius);
+        System.out.println("Celsius is"+Celsius);
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+}//class
 
 
 
@@ -11,4 +41,3 @@ public class Quiz3 {
 
 
 
-}
